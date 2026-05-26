c7248ade6b17713dd5ea410416e5a674feef7513
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-05-26 10:04:31 +0530

    intial commit: With the ApiResponse common property

---
+++ src/main/java/com/example/edustream_lib_common/dto/ApiResponse.java
@@ -0,0 +1,30 @@
+package com.example.edustream_lib_common.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+
+    public  static <T> ApiResponse<T> success(T data) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .data(data)
+                .build();
+    }
+
+    public  static <T> ApiResponse<T> success(T data, String message) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .message(message)
+                .data(data)
+                .build();
+    }
+}
---
+++ src/test/java/com/example/edustream_lib_common/EdustreamLibCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edustream_lib_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdustreamLibCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
